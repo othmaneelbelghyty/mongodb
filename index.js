d7e6b815419b8faa2f5e472f44dc5e02e364a1c1
othmaneelbelghyty <othmane.elbelghyty@gmail.com>
2024-01-01 19:33:09 +0100

    mongodb

---
+++ index.js
@@ -0,0 +1,32 @@
+db.contactlist.insert({
+  last_name: "Ben Lahmer",
+  first_name: "Fares",
+  email: "fares@gmail.com",
+  age: 26,
+});
+
+db.contactlist.insert({
+  last_name: "Kefi",
+  first_name: "Seif",
+  email: "kefi@gmail.com",
+  age: 15,
+});
+
+db.contactlist.insert({
+  last_name: "Fatnassi",
+  first_name: "Sarra",
+  email: "sarra.f@gmail.com",
+  age: 40,
+});
+
+db.contactlist.insert({
+  last_name: "Ben Yahia",
+  first_name: "Rym",
+  age: 4,
+});
+
+db.contactlist.insert({
+  last_name: "Cherif",
+  first_name: "Sami",
+  age: 3,
+});
